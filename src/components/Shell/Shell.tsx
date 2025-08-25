f0c73c1044390a215f4e5c41b4667c267fa33da7
marromugi <marrromugi@gmail.com>
2025-08-26 02:07:53 +0900

    fix motion import

--- src/components/Shell/Shell.tsx
+++ src/components/Shell/Shell.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { motion } from 'framer-motion';
+import { motion } from 'motion/react';
 import { ShellProps } from './type';
 
 export const Shell = ({ children }: ShellProps) => {
